bf61106a16fa1ddebd798ccb9993c13f580129e1
dimiana17 <93286131+dimiana17@users.noreply.github.com>
2025-05-08 15:16:24 +0300

    update reviews remove package id

--- models/reviews.js
+++ models/reviews.js
@@ -5,7 +5,7 @@ const reviewSchema = new mongoose.Schema({
     rate: { type: Number, min: 1, max: 5 },
     user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
     vendor_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Vendor' },
-    package_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Package' },
+    // package_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Package' },
     date: { type: Date, default: Date.now },
     verified: { type: Boolean, default: false }
   });
