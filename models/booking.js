2db32fba4695424d2aca7d3601c5d2e13e39ef8a
dimiana17 <93286131+dimiana17@users.noreply.github.com>
2025-05-08 14:57:31 +0300

    fix booking model

--- models/booking.js
+++ models/booking.js
@@ -1,13 +1,24 @@
-const mongoose=require('mongoose');
+const mongoose = require("mongoose");
 
-const paymentSchema = new mongoose.Schema({
-    method: String,
-    amount: Number,
-    payment_status: { type: String, enum: ['pending', 'paid', 'failed'], default: 'pending' },
-    date: { type: Date, default: Date.now },
-    user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-    vendor_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Vendor' },
-    booking_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Booking' }
-  });
+const bookingSchema = new mongoose.Schema({
+  user_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  vendor_id: { type: mongoose.Schema.Types.ObjectId, ref: "Vendor" },
+  package_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Package",
+    required: true,
+  },
+  payment_id: { type: mongoose.Schema.Types.ObjectId, ref: "Payment" },
+  date: Date,
+  status: {
+    type: String,
+    enum: ["pending", "confirmed", "cancelled"],
+    default: "pending",
+  },
+});
 
-module.exports = mongoose.model("Payment", paymentSchema);
+module.exports = mongoose.model("Booking", bookingSchema);
