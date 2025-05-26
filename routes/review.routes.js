831f992d65935e7867eeb14a5a8c88e54741a871
dimiana17 <93286131+dimiana17@users.noreply.github.com>
2025-05-26 03:01:25 +0300

    controllers in reviews

--- routes/review.routes.js
+++ routes/review.routes.js
@@ -2,10 +2,16 @@ const express = require('express');
 const router = express.Router();
 const {
   createReview,
-  getReviewsByVendorId
+  getReviewsByVendorId,
+  getReviewsByServiceId,
+  getTopRatedVendors,
+  getTopRatedVendorsWithTopServiceByCategory
 } = require('../controllers/reviewsController');
 
-
+router.get('/top-rated/category', getTopRatedVendorsWithTopServiceByCategory);
+router.get('/top-rated', getTopRatedVendors);
 router.post('/', createReview);
 router.get('/:vendorId', getReviewsByVendorId);
+router.get('/service/:serviceId', getReviewsByServiceId);
+
 module.exports = router;
